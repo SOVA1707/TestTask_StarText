59b9d50f1f70a279643cf7e131a5755b21f74737
SOVA1707 <81626778+SOVA1707@users.noreply.github.com>
2022-03-06 08:07:54 +0700

    Find and mark error in ArtifactService.updateArtifact()

--- src/main/java/com/startext/testtask/service/ArtifactService.java
+++ src/main/java/com/startext/testtask/service/ArtifactService.java
@@ -26,8 +26,8 @@ public class ArtifactService {
     }
 
     public Artifact getArtifact(UUID id) throws ArtifactNotFoundException {
-        //If findBySuuid is executed first, then findById will be executed.
-        //Otherwise, findById will not find anything.
+        //If findBySuuid() is executed first, then findById will be executed.
+        //Otherwise, findById() will not find anything.
         //Check uuid id annotation.
         artifactRepository.findBySuuid(id.toString());
         Optional<ArtifactEntity> optionalArtifact = artifactRepository.findById(id);
@@ -39,11 +39,13 @@ public class ArtifactService {
     }
 
     public Artifact updateArtifact(UUID id, Artifact newArtifact) throws ArtifactNotFoundException {
+        artifactRepository.findBySuuid(id.toString());
         Optional<ArtifactEntity> optionalArtifact = artifactRepository.findById(id);
         if (optionalArtifact.isPresent()) {
             ArtifactEntity artifactEntity = optionalArtifact.get();
             artifactEntity.setCategory(newArtifact.getCategory());
             artifactEntity.setDescription(newArtifact.getDescription());
+            // HHH000010: On release of batch it still contained JDBC statements
             artifactRepository.save(artifactEntity);
             return Artifact.toModel(artifactEntity);
         } else {
