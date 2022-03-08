b246d4eea71e7d5dcd69b772e4ff95adab75f442
SOVA1707 <81626778+SOVA1707@users.noreply.github.com>
2022-03-08 10:47:50 +0700

    null artifact entity

--- src/test/java/com/startext/testtask/service/ArtifactServiceTest.java
+++ src/test/java/com/startext/testtask/service/ArtifactServiceTest.java
@@ -1,44 +1,84 @@
 package com.startext.testtask.service;
 
 import com.startext.testtask.entity.ArtifactEntity;
+import com.startext.testtask.model.Artifact;
+import com.startext.testtask.repository.ArtifactRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.util.UUID;
 
-@DataJpaTest
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class ArtifactServiceTest {
 
-//    @Autowired
-//    private ArtifactService artifactService;
+    @Mock
+    private ArtifactRepository artifactRepository;
 
-    @Autowired
-    private TestEntityManager entityManager;
+    @Mock
+    private ArtifactService artifactService;
+
+    @Mock
+    private ArtifactEntity artifactEntity1;
+
+    @BeforeAll
+    static void init() {
+    }
 
     @DisplayName("Create artifact test")
     @Test
     void createArtifact() {
-        ArtifactEntity artifactEntity = entityManager.persist(new ArtifactEntity());
+        System.out.println("Entity:");
+        System.out.println(artifactEntity1);
+        System.out.println(artifactEntity1.getId());
         assertDoesNotThrow(() -> {
-//            artifactService.createArtifact(artifactEntity);
+            artifactService.createArtifact(artifactEntity1);
         });
     }
 
     @DisplayName("Get artifact test")
     @Test
     void getArtifact() {
+        assertDoesNotThrow(() -> {
+            artifactService.createArtifact(artifactEntity1);
+            Artifact artifact = artifactService.getArtifact(artifactEntity1.getId());
+            assertNotNull(artifact);
+        });
     }
 
     @DisplayName("Update artifact test")
     @Test
     void updateArtifact() {
+        assertDoesNotThrow(() -> {
+            Artifact newArtifact = new Artifact();
+            String newId = "123";
+            newArtifact.setUserId("123");
+            String newCategory = "newCategory";
+            newArtifact.setCategory(newCategory);
+            String newDescription = "newDescription";
+            newArtifact.setDescription(newDescription);
+
+            artifactService.createArtifact(artifactEntity1);
+            Artifact artifact = artifactService.updateArtifact(artifactEntity1.getId(), newArtifact);
+
+            assertNotNull(artifact);
+            assertEquals(artifact.getUserId(), newId);
+            assertEquals(artifact.getCategory(), newCategory);
+            assertEquals(artifact.getDescription(), newDescription);
+        });
     }
 
     @DisplayName("Delete artifact test")
     @Test
     void deleteArtifact() {
+        assertDoesNotThrow(() -> {
+            artifactService.createArtifact(artifactEntity1);
+            Artifact artifact = artifactService.deleteArtifact(artifactEntity1.getId());
+            assertNotNull(artifact);
+        });
     }
 }
