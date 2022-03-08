a7846b365ed5366c3ea9ddd8d8497b05914a5a14
SOVA1707 <81626778+SOVA1707@users.noreply.github.com>
2022-03-08 10:53:40 +0700

    prev update

--- src/test/java/com/startext/testtask/service/ArtifactServiceTest.java
+++ src/test/java/com/startext/testtask/service/ArtifactServiceTest.java
@@ -7,16 +7,18 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class ArtifactServiceTest {
 
-    @Mock
+    @Autowired
     private ArtifactRepository artifactRepository;
 
     @Mock
