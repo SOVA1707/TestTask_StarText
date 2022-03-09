acc7f64cbe714eb1807a256930f3540220d4e5de
SOVA1707 <81626778+SOVA1707@users.noreply.github.com>
2022-03-09 20:19:33 +0700

    save commit

--- src/main/java/com/startext/testtask/adapter/CustomWebSecurityConfigureAdapter.java
+++ src/main/java/com/startext/testtask/adapter/CustomWebSecurityConfigureAdapter.java
@@ -4,17 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
+public class CustomWebSecurityConfigureAdapter extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
